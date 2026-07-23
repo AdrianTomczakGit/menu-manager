27e3fcb8e9f44821713c05c78c515d3320a28339
Adrian <254021182+adinadiltd-cyber@users.noreply.github.com>
2026-07-23 17:13:52 +0100

    Add initial Menu Manager homepage

---
+++ src/test/java/com/adriantomczak/menumanager/MenuManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriantomczak.menumanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
